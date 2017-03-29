0d0010b61f33c97bc37bb3a7a88ce464872db5f5
nikolemc <nikolemcstanley@gmail.com>
2017-03-29 15:28:40 -0400

    read the Morse Code CSV into a dictionary

--- MorseCodeConverter/MorseCodeConverter/Program.cs
+++ MorseCodeConverter/MorseCodeConverter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,31 @@ namespace MorseCodeConverter
 {
     class Program
     {
+        const string path = "morse.csv"; //this is where the Morse Code file lives
+        Dictionary<char, string> ConvertDictionary = new Dictionary<char, string>();
+
+        //Add Elements to Dictionary
+        public static Dictionary<char, string> ReadMorseCodeFromFile()
+        {
+            var rv = new Dictionary<char, string>();
+            using (var reader = new StreamReader(path))
+            {
+                while (reader.Peek() > -1)
+                {
+                    var line = reader.ReadLine().Split(',') ; // ["A",".-."]
+                    rv.Add(char.Parse(line[0]),line[1]);
+                }
+            }
+            return rv;
+        }
+
         static void Main(string[] args)
         {
+          
         }
+
+        
+ 
+        
     }
 }
